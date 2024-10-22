8390094421d621936c88872db9acd0f54b3abaa7
Honsei901 <homma_wic@outlook.jp>
2024-10-22 23:54:47 +0900

    Added tailwindcss into this app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,10 +3,13 @@ import { Provider } from 'react-redux';
 
 import User from './components/User';
 import Counter from './components/Counter';
+import AppRouter from './routers/AppRouter';
 
 const App = () => {
   return (
     <div>
+      <AppRouter />
+
       <Provider store={store}>
         <Counter />
         <User />
